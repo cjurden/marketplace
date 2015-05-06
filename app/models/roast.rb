004e16c33d03e81d641f854575ed184ec2878320
cjurden <colejurden@gmail.com>
2015-05-06 13:35:22 -0500

    added sales db

--- app/models/roast.rb
+++ app/models/roast.rb
@@ -1,10 +1,10 @@
 class Roast < ActiveRecord::Base
 	extend FriendlyId
   friendly_id :name, use: :slugged
-  
+
 	belongs_to :user
 	has_attached_file :image
-	
+	has_many :sales
 	validates_attachment_content_type :image,
 	content_type: /^image\/(png|gif|jpeg)/,
 	message: "Only images allowed"
