be2648763d8122e843f3fcd3cdb26654e293a4c8
cjurden <colejurden@gmail.com>
2015-05-05 23:21:52 -0500

    made dashboard

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,7 +1,12 @@
 class PagesController < ApplicationController
+	before_action :authenticate_user!, only: [:dashboard]
   def home
   	if current_user
   		redirect_to roasts_path
   	end
+  end
+
+  def dashboard
+  	@roasts = current_user.roasts
   end
 end
