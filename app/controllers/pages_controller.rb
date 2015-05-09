5fd27e6916a73ba072da786f1dc6ffa27b3fb6a9
BrandonT <Magp13s43ver@users.noreply.github.com>
2015-05-09 18:59:18 -0500

    Update pages_controller.rb
    Added comments

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,8 @@
 class PagesController < ApplicationController
+	#Authenticate user is signed in before showing dashboard
 	before_action :authenticate_user!, only: [:dashboard]
+	
+  #Function to view roasts index	
   def home
   	if current_user
   		redirect_to roasts_path
@@ -7,6 +10,7 @@ class PagesController < ApplicationController
   	@roasts = Roast.where(available: true).last(4)
   end
 
+  #Function to view dashboard
   def dashboard
   	@roasts = current_user.roasts
   	@purchased = Sale.where(buyer_email: current_user.email)
