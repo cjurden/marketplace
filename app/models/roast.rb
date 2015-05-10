cdcc1eef5c0b54e254dac57f5a73a0259ca8bc72
cjurden <colejurden@gmail.com>
2015-05-10 01:49:32 -0500

    Document roast.rb

--- app/models/roast.rb
+++ app/models/roast.rb
@@ -1,13 +1,18 @@
 class Roast < ActiveRecord::Base
+	#code for better looking hyperlink
 	extend FriendlyId
     friendly_id :name, use: :slugged
-
+    
+	#attributes of roast
 	belongs_to :user
 	has_attached_file :image
 	has_many :sales
+	
+	#image upload validation
 	validates_attachment_content_type :image,
 	content_type: /^image\/(png|gif|jpeg)/,
 	message: "Only images allowed"
 	
+	#requires image to be included in a new roast
 	validates :image, attachment_presence: true
 end
